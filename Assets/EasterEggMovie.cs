17d6731147f9f1f0449033f322335c8bc27f332d
Lucas Burdell <lucas.burdell@blackburn.edu>
2018-01-27 04:01:54 -0600

    fix easter egg

--- Assets/EasterEggMovie.cs
+++ Assets/EasterEggMovie.cs
@@ -10,6 +10,8 @@ public class EasterEggMovie : MonoBehaviour {
 
     VideoPlayer player;
     public Transform character;
+
+    int playTimer = 0;
     //Material meshMaterial;
 
     int alpha = 0x00;
@@ -33,11 +35,21 @@ public class EasterEggMovie : MonoBehaviour {
             var color = material.color;
             var newColor = new Color(color.r, color.g, color.b, Mathf.Clamp(((float) alpha) / 60.0f, 0.0f, 1.0f));
             material.color = newColor;
+            if (Input.GetKey(KeyCode.R)) {
+                player.Stop();
+                alpha = 0;
+                material = GetComponent<Renderer>().material;
+                color = material.color;
+                newColor = new Color(color.r, color.g, color.b, Mathf.Clamp(((float)alpha) / 60.0f, 0.0f, 1.0f));
+                material.color = newColor;
+                playTimer = 10;
+            }
         } else {
+            playTimer = Mathf.Clamp(playTimer - 1, 0, 10);
             var distance = Vector3.Distance(gameObject.transform.position, character.position);
             var input = Input.GetKey(KeyCode.R);
             
-            if (input) {
+            if (input && playTimer == 0) {
                 
                 if (distance <= 5) {
                     player.isLooping = true;
